11f790144e4ec84c0e3db14a173b8681f9634f8e
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-07 23:01:08 +0530

    html elements created

--- BMI/src/App.jsx
+++ BMI/src/App.jsx
@@ -1,7 +1,31 @@
 const App = () => {
   return (
     <>
-    
+      <div className="Container">
+        <div className="Section-1">
+          <img src="" alt="" />
+        </div>
+        <div className="section-2">
+          <h1>BMI CALCULATOR</h1>
+          <div className="height-box">
+            <label htmlFor="height">Height (cm)</label>
+            <input type="text" id="height" />
+          </div>
+          <div className="weight-box">
+            <label htmlFor="weight">Weight (kg)</label>
+            <input type="text" id="weight" />
+          </div>
+          <div>
+            <button>
+              Calculate BMI
+            </button>
+          </div>
+          <div>
+            <h4>Your BMI is : 20 </h4>
+            <p> Status : Over Weight</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
